ef75c388648f2f040d5df5bcb083e02c797ff4cf
Richard Norling <rich.norling@gmail.com>
2022-03-18 09:35:41 +0100

    Remove trailing whitespaces and roles/i3/files/i3blocks

--- roles/i3/files/i3blocks/blocks/datetime.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-DAY_MONTH=$(date +"%d/%m")
-HOUR_MINUTE=$(date +"%H:%M")
-
-echo " ${DAY_MONTH} |  ${HOUR_MINUTE}"
--- roles/i3/files/i3blocks/blocks/essid.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ESSID=$(/sbin/iwconfig | perl -n -e'/ESSID:"(.*?)"/ && print $1')
-
-echo " $ESSID"
