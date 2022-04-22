89e74a644a2e2ed466c6a5fcec688f164b87c54b
Richard Norling <rich.norling@gmail.com>
2022-04-23 00:14:05 +0200

    Add a i3 restart after switching screenlayouts

--- roles/i3/files/i3/arandr-profile-switcher.sh
+++ roles/i3/files/i3/arandr-profile-switcher.sh
@@ -9,4 +9,7 @@ selected_profile=$(zenity --list --title 'Arandr Profile Switcher' --column='0' 
 # execute xrandr layout file
 ~/.screenlayout/${selected_profile}
 
+# restart i3
+i3-msg restart
+
 exit 0
